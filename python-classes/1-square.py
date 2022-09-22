ba7f76914791dff489b836d9032117f287bfc4ec
ayaaribi123 <ayaaribi11@gmail.com>
2022-09-22 17:55:03 +0100

    class have a private instance attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that have Private instance attribute size."""
+
+
+class Square:
+    """the Private instance attribute size."""
+
+    def __init__(self, size):
+        self.__size = size
