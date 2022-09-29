56fd799386a321c74bda29edd271f56405170a7c
ayaaribi123 <ayaaribi11@gmail.com>
2022-09-29 10:08:44 +0100

    check type

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that returns True if the object is
+ same type as class ;
+ otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """a function that returns True if the object
+    is same type as  class ;
+    otherwise False"""
+    if type(obj) == a_class:
+            return True
+    else:
+            return False
