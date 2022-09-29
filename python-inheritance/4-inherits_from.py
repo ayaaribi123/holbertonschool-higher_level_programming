3107e1bdd767e7c08cc8bda3ef3839114fec09df
ayaaribi123 <ayaaribi11@gmail.com>
2022-09-29 11:01:22 +0100

    check

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" returns True if the object is an 
+instance of a class that inherited"""
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an 
+	instance of a class that inherited"""
+    if isinstance(obj, a_class):
+        return False
+    else:
+        return True
