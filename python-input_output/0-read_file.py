9cbbad8f215ada01885997aba4cf71312204e802
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-02 17:13:17 +0100

    open and read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """read file"""
-    with open("my_file_0.txt", "r", encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, "r", encoding='utf-8') as filename:
+        print(filename.read())
