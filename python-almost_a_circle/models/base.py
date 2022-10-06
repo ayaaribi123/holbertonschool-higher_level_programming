745ef67ff71db268783aaa4ef2d939ff4749a691
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-06 15:58:46 +0100

    base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Base"""
+
+
+class Base:
+
+    """class base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        function that create an object from json file
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
