c99b12bc68d54a27bff6c4ceb71cfb39107362a3
ayaaribi123 <ayaaribi11@gmail.com>
2022-10-09 15:21:30 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -116,3 +116,8 @@ class Rectangle(Base):
                     self.x = kwargs["x"]
                 if i == "y":
                     self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """to dictionary"""
+        return {"x": self.x, "y": self.y,
+		 "id": self.id, "height": self.height, "width": self.width}
