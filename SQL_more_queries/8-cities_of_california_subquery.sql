1bec71fa10634a47dee235ed1ce865c5238afaca
ayaaribi123 <ayaaribi11@gmail.com>
2022-11-08 23:23:30 +0100

    list the crities of ccalifornia that can be found in the database

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT  name, id
+FROM cities
+WHERE state_id=(
+	SELECT id
+	FROM states
+	WHERE name ="california"
+)
+ORDER BY cities.id ASC
