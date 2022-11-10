77e052a88f2df25dc876c6a7b830cf546a897bb2
ayaaribi123 <ayaaribi11@gmail.com>
2022-11-10 21:19:39 +0100

    import the database dump

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT tv_genres.name AS name,
+SELECT tv_genres.name AS "name",
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id= tv_genres.id
