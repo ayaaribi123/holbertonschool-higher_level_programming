97536208a43cbbddb74c0935b76f27e5442a95b8
ayaaribi123 <ayaaribi11@gmail.com>
2022-11-10 22:16:18 +0100

    import the database dump

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,5 +3,4 @@ SELECT tv_shows.title AS "title", tv_genres.name AS "name"
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
 ORDER BY title, name
