9b90898685887c8420302874926139ea834d2fb4
ayaaribi123 <ayaaribi11@gmail.com>
2022-11-10 21:12:18 +0100

    import the database dump

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
 SELECT tv_genres.name AS name,
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_show_genres.show_id= tv_show.id
+INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id= tv_genres.id
 WHERE tv_show.title = Dexter
-ORDER BY name
+ORDER BY name ASC
