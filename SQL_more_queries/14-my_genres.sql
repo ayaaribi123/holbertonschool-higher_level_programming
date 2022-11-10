63bc51b80c623080320d0478c46d3fa973452c17
ayaaribi123 <ayaaribi11@gmail.com>
2022-11-10 21:17:29 +0100

    import the database dump

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 INNER JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id= tv_genres.id
 WHERE tv_show.title = Dexter
-ORDER BY name ASC
+ORDER BY name
