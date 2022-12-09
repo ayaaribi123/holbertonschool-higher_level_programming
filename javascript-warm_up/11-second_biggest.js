ffd1db376622da478ba078e534019ee3bb705c3c
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-09 19:50:23 +0100

    second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const len = process.argv.length;
+if (len <= 3) {
+  console.log(0);
+} else {
+  const arr = process.argv.map(Number);
+  arr.sort((a, b) => a - b);
+  arr.shift();
+  console.log(arr[arr.length - 2]);
+}
