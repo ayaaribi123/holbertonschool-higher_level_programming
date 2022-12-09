97eb6426492fc7195cbf38830b1b9db7dc6ff426
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-09 13:14:18 +0100

    saquare

--- javascript-warm_up/8-saquare.js
+++ javascript-warm_up/8-saquare.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2])
-let i = 0
-if(num){
-	for(i; i < num; i++)
-	{
-		console.log("x".repeat(num));
-	}
-}
-else
-{
-	console.log("Missing size")
+const num = parseInt(process.argv[2]);
+let i = 0;
+if (num) {
+  for (i; i < num; i++) {
+    console.log('x'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
 }
