42a596aa901ea408c296d4c4c54d587daac3535d
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-09 14:42:08 +0100

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 function factorial (x) {
-  if (isNaN(x) || x === 1) {
+  if (isNaN(x)) {
+    return 1;
+  } else if (x === 0) {
     return 1;
   } else {
     return x * factorial(x - 1);
   }
 }
-console.log(factorial(parseInt(process.argv[2])));
+const x = parseInt(process.argv[2]);
+console.log(factorial(x));
