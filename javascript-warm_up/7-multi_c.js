1cb461ab982e19b673fae0c2bb11133ed76136fc
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-09 13:13:12 +0100

    multi_c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-let i = 0
-const num = parseInt(process.argv[2])
-if (num)
-{
-	for(i; i < num; i++)
-	{
-		console.log("C is fun")
-	}
+let i = 0;
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (i; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
-	else 
-	{
-		console.log("Missing number of occurrences")
-	}
