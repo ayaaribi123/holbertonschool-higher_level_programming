74aea5eee68272950467ce3110a0c085add05f04
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-09 12:09:15 +0100

    integre

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2])
-if (Math.floor(num) == NaN)
-{
-console.log("Not a number");
-}
-else
-{
-console.log("My number: " + num);
+const num = parseInt(process.argv[2]);
+if (Math.floor(num) === isNaN) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
