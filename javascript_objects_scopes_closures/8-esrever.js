1f2839ef659347811a700a10b50f91205fb2b854
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-14 02:46:15 +0100

    eserver

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const resualt = [];
-		let i = 0;
-		for (i; i = list.length; i++) {
-			resualt.push(list.pop());
-	}
-	return resualt;
+  let i = 0;
+  for (i; list.length; i++) {
+    resualt.push(list.pop());
+  }
+  return resualt;
 };
