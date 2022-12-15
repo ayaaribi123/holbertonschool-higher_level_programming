116d2fce24c1fd3e61464279499ba492850f7891
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-15 01:52:36 +0100

    readfile

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.error(err);
-  } else{
+  } else {
     console.log(data);
   }
-	});
+});
