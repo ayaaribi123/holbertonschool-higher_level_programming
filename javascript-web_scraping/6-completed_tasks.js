032b5eef8874e4e7a9c400fc08698321d3455bbb
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-16 19:48:33 +0100

    completed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,14 +7,12 @@ request.get(url, function (err, resualt, body) {
     const content = JSON.parse(body);
     const completed = {};
     for (const i of content) {
-      if (i.completed === undefined) {
-        if (completed[i.userId] === undefined) {
-          completed[i.userId] = 1;
-        } else {
+        if (content in completed) {
           completed[i.userId] = completed[i.userId] + 1;
+        } else {
+          completed[i.userId] = 1;
         }
       }
     }
     console.log(completed);
-  }
-});
+			});
