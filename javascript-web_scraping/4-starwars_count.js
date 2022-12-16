98fb8aff43df4498fc3e4956791704afdb8926ec
ayaaribi123 <ayaaribi11@gmail.com>
2022-12-16 18:14:02 +0100

    movires

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,6 +4,6 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 request.get(url, function (err, resualt, body) {
   if (err) throw err;
   const movies = JSON.parse(body).resualt;
-  const mv = (movies.reduce(movie => movie.characters.includes('/18')));
+  const mv = (movies.filter(movie => movie.characters.includes('/18')));
   console.log(mv.length);
 });
