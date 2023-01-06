70b29145e57d351986156d43d0f20938e2375137
ayaaribi123 <ayaaribi11@gmail.com>
2023-01-06 13:45:15 +0100

    task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  } else if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+	if (isNaN(n)) {
+			return 1;
+	} else if (n === 0) {
+			return 1;
+	} else {
+			return n * factorial(n - 1);
+	}
 }
 
 const n = parseInt(process.argv[2]);
